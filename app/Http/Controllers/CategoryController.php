e80eb8c42b0d5ebe2277182e62df3e9e9bf5a6cf
Anwaar <malikanwaar@gmail.com>
2025-09-11 15:33:15 +0100

    =add categories url

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -60,7 +60,7 @@ class CategoryController extends Controller
     public function create()
     {
         //
-        return view("addcategory")->with(["url"=>"category-save"]);
+        return view("addcategory")->with( [ "url"=>"category-save" ] );
     }
 
     /**
